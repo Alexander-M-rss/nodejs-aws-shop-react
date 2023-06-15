07ea5f417e91f5a1edefc552eeee19baf95e54bb
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2023-06-15 18:32:54 +0300

    feat: integrate import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://7uxztr6cn6.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://xq1g7x4qxa.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://7uxztr6cn6.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
