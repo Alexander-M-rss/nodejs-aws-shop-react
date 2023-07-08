6faf8deda7bedd123a91899ac92801563fb40ec6
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2023-07-08 12:46:13 +0300

    fix: API_PATHS for carts service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://xq1g7x4qxa.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://7uxztr6cn6.execute-api.eu-west-1.amazonaws.com",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://ofn2s2tt56.execute-api.eu-west-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
