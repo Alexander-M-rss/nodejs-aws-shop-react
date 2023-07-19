93d7227ef91477b70cfe7c8d5d0eda091db35f7f
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2023-07-19 19:42:01 +0300

    feat: integrate bff-service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,7 @@
 const API_PATHS = {
-  product: "https://7uxztr6cn6.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://xq1g7x4qxa.execute-api.eu-west-1.amazonaws.com/prod",
-  bff: "https://7uxztr6cn6.execute-api.eu-west-1.amazonaws.com",
-  cart: "http://alexander-m-rss-cart-api-prod.eu-west-1.elasticbeanstalk.com/api",
+  bff: "https://prm9wvc3z6.execute-api.eu-west-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
